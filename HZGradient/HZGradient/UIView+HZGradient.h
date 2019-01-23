e0449db812247b4448ae43ee5930aa52d08283c5
heze <895315401@qq.com>
2019-01-23 11:35:29 +0800

    渐变颜色控件

---
+++ HZGradient/HZGradient/UIView+HZGradient.h
@@ -0,0 +1,17 @@
+//
+//  UIView+HZGradient.h
+//  HZGradient
+//
+//  Created by 何泽的mac on 2019/1/23.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (HZGradient)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HZGradient/HZGradient/UIView+HZGradient.m
@@ -0,0 +1,13 @@
+//
+//  UIView+HZGradient.m
+//  HZGradient
+//
+//  Created by 何泽的mac on 2019/1/23.
+//  Copyright © 2019年 何泽的mac. All rights reserved.
+//
+
+#import "UIView+HZGradient.h"
+
+@implementation UIView (HZGradient)
+
+@end
